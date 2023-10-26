487e8e55f3883a920adbde1ed445bdce2e52e0f4
Egor Zhdan <e_zhdan@apple.com>
2023-10-26 19:31:45 +0100

    [cxx-interop] Allow removing elements from `std::vector`
    This adds `func remove(at index: Int)` to all instantiations of `std::vector` via an extension for `protocol CxxVector`.
    
    The original C++ method `std::vector::erase` is not visible in Swift because all of its overloads return unsafe iterators.
    
    rdar://113704853

--- stdlib/public/Cxx/cxxshim/libcxxstdlibshim.h
+++ stdlib/public/Cxx/cxxshim/libcxxstdlibshim.h
@@ -1,5 +1,8 @@
 #include <functional>
 #include <string>
+#ifdef _WIN32
+#include <vector>
+#endif
 
 /// Used for std::string conformance to Swift.Hashable
 typedef std::hash<std::string> __swift_interopHashOfString;
@@ -9,3 +12,9 @@ typedef std::hash<std::u16string> __swift_interopHashOfU16String;
 
 /// Used for std::u32string conformance to Swift.Hashable
 typedef std::hash<std::u32string> __swift_interopHashOfU32String;
+
+#ifdef _WIN32
+template <class T>
+ptrdiff_t operator-(const std::_Vector_iterator<T> &lhs,
+                    const std::_Vector_iterator<T> &rhs);
+#endif
